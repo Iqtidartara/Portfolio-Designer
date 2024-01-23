66d8a333dc204aed0bbbeade3e3f26a543144a6e
Iqtidartara <135112726+Iqtidartara@users.noreply.github.com>
2024-01-23 17:20:53 +0500

    refractor: responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HomeSection from "@/components/HomeSection";
+import RecentClients from "@/components/RecentClients";
 
 export default function Home() {
   return (
    <div>
     <HomeSection />
+    <RecentClients />
    </div>
   );
 }
---
+++ components/RecentClients.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RecentClients = () => {
+  return (
+    <section className='w-full'>
+        <div className='mx-auto max-w-[70rem] '>
+            <h2 className='text-[2.625rem] text-[#080415] leading-[119.048%] font-[500]'>Meet my <br /> recent clients</h2>
+            </div></section>
+  )
+}
+
+export default RecentClients
